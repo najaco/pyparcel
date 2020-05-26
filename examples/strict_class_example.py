eeecb106fb1798f8092aab50d4c0755a65e57aca
najaco <ncohen4299@gmail.com>
2020-05-26 16:55:39 -0400

    Updated example

--- examples/strict_class_example.py
+++ examples/strict_class_example.py
@@ -2,7 +2,7 @@ from pyparcel import Long, UnsignedLongLong, Double
 
 
 class Foo:
-    def __init__(self, a: int, b: int, c: int, d: int):
+    def __init__(self, a: int, b: int, c: int, d: long):
         self.a = a
         self.b = Long(b)
         self.c = UnsignedLongLong(c)
