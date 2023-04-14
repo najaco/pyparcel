0c611765408871c7df1d6442df97a147eca0d7ea
Nathan Cohen <ncohen4299@gmail.com>
2023-04-14 10:17:45 -0700

    Added test cases to test that encodings are checked properyly

--- tests/test_string.py
+++ tests/test_string.py
@@ -18,7 +18,19 @@ DATA: List[str] = [
     "a" * 2 ** 20,
     " " * 2 ** 20,
 ]
+
 ENCODINGS: List[str] = ["utf-8", "utf-16", "utf-32", "ascii"]
+FAKE_ENCODINGS: List[str] = [
+    "",
+    " ",
+    "fake_encoding1",
+    "utf-9",
+    "utf-15",
+    "utf-17",
+    "Hello World",
+    "ASCI",
+    "a" * 100
+]
 
 
 class TestString(unittest.TestCase):
@@ -50,6 +62,14 @@ class TestString(unittest.TestCase):
                 self.assertEqual(
                     s, pyparcel.unpack(pyparcel.pack(s, encoding=e), str(), encoding=e)
                 )
+
+    def test_pack_invalid_encoding_throws_exception(self):
+        for e in FAKE_ENCODINGS:
+            self.assertRaises(Exception, pyparcel.pack, DATA[6], encoding=e)
+
+    def test_unpack_invalid_encoding_throws_exception(self):
+        for e in FAKE_ENCODINGS:
+            self.assertRaises(Exception, pyparcel.unpack, pyparcel.pack(DATA[6]), str(), encoding=e)
 
 
 if __name__ == "__main__":
